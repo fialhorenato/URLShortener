e3b5369f922a71cceac9027bf6c6d89cc5f57571
Renato da Silva Reis Fialho <renato@renatofialho.com>
2019-01-19 23:26:23 -0200

    Improvements

--- src/main/java/com/urlshortener/controller/UrlController.java
+++ src/main/java/com/urlshortener/controller/UrlController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
 import com.urlshortener.model.UrlDTO;
 import com.urlshortener.model.UrlModel;
@@ -26,9 +27,13 @@ public class UrlController {
 	private UrlService urlService;
 
 	@GetMapping(value = "/{id}", produces = MediaType.ALL_VALUE)
-	public ResponseEntity<UrlModel> redirectToSavedUrl(@PathVariable String id) {
+	public RedirectView redirectToSavedUrl(@PathVariable String id) {
 		Optional<UrlModel> urlModel = urlService.findById(id);
-		return mountResponseEntityToRedirect(urlModel);
+		if (urlModel.isPresent()) {
+			return new RedirectView(urlModel.get().getUrl());
+		} else {
+			return new RedirectView();
+		}
 	}
 
 	@PostMapping(value = "/", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
@@ -40,16 +45,6 @@ public class UrlController {
 		return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 
-	private ResponseEntity<UrlModel> mountResponseEntityToRedirect(Optional<UrlModel> urlModel) {
-		if (urlModel.isPresent() && UrlUtils.isUrlValid(urlModel.get().getUrl())) {
-			HttpHeaders headers = new HttpHeaders();
-			headers.setLocation(UrlUtils.getURIFromUrl(urlModel.get().getUrl()));
-			// NEED NO CACHE CONTROL FOR THE STATISTICS, IT CANNOT CACHE THE REQUEST TO COUNT THE HITS
-			headers.setCacheControl(CacheControl.noCache());
-			return new ResponseEntity<>(headers, HttpStatus.PERMANENT_REDIRECT);
-		} else {
-			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-		}
-	}
+
 
 }
